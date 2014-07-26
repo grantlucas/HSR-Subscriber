ed8d259f9adb05bbcca385cac2f3f0df39b1bc1f
Grant Lucas <contact@grantlucas.com>
2014-07-26 15:50:14 -0400

    Have limited natural language parsing complete with validation of needed params

--- src/Controller/Subscribe.php
+++ src/Controller/Subscribe.php
@@ -91,5 +91,101 @@ class Subscribe
         $app = \Slim\Slim::getInstance();
 
         $twilio = new \Services_Twilio($app->user_config['twilio']['sid'],$app->user_config['twilio']['secret']);
+
+        // Twilio params that are sent
+        // Body
+        // From
+
+        $message     = isset($_GET['Body']) ? $_GET['Body'] : null;
+        $message     = "Subscribe to stop ";
+        $from_number = isset($_GET['From']) ? $_GET['From'] : null;
+
+        echo "<xmp style='background-color: #fff;'>";
+        print_r($message);
+        echo "</xmp>";
+
+        // Parse out the stop number from the message
+        $stop_pattern = "/stop\ *([1-9]+)/i";
+        $stop_matches = array();
+        preg_match($stop_pattern, $message, $stop_matches);
+        $stop_number = isset($stop_matches[1]) ? $stop_matches[1] : "";
+
+        echo "<xmp style='background-color: #fff;'>";
+        print_r("Stop Number: " . $stop_number);
+        echo "</xmp>";
+
+        // Parse out the route number from the message
+        $route_pattern = "/route\ *([1-9]+)/i";
+        $route_matches = array();
+        preg_match($route_pattern, $message, $route_matches);
+        $route_number = isset($route_matches[1]) ? $route_matches[1] : "";
+
+
+        echo "<xmp style='background-color: #fff;'>";
+        print_r("Route Number: " . $route_number);
+        echo "</xmp>";
+
+        // Parse out the time from the message
+        $time_pattern = "/([1-9][0-9]?:*[0-9+]*\ *(?:am|pm))/i";
+        $time_matches = array();
+        preg_match($time_pattern, $message, $time_matches);
+        $time = isset($time_matches[1]) ? $time_matches[1] : "";
+
+        echo "<xmp style='background-color: #fff;'>";
+        print_r("Time: " . $time);
+        echo "</xmp>";
+
+        // Parse out the day of the week
+        $day_pattern = "/(monday|tuesday|wednesday|thursday|friday|saturday|sunday|mon|tues|wed|thurs|fri|sat|sun)/i";
+        $day_matches = array();
+        preg_match($day_pattern, $message, $day_matches);
+        $day = isset($day_matches[1]) ? $day_matches[1] : "";
+
+        echo "<xmp style='background-color: #fff;'>";
+        print_r("Day: " . $day);
+        echo "</xmp>";
+
+        $missing_data = array();
+
+        if($stop_number == "")
+            $missing_data[] = "stop number";
+
+        if($route_number == "")
+            $missing_data[] = "route number";
+
+        if($time == "")
+            $missing_data[] = "time of day";
+
+        if($day == "")
+            $missing_data[] = "day of the week";
+
+        if(!empty($missing_data)) {
+            $response = "We are missing some data for your request. Please be sure to include the ";
+            $response .= implode(", ", $missing_data);
+            $response .= " in your request";
+
+            try {
+                $message = $twilio->account->messages->sendMessage(
+                    $app->user_config['twilio']['number'], // From a valid Twilio number
+                    $from_number, // Text this number
+                    $response
+                );
+                print $message->sid;
+            } catch (\Services_Twilio_RestException $e) {
+                print $e->getMessage();
+                print $e->getCode();
+
+                switch ($e->getCode())
+                {
+                case '21610':
+                    // User unsubscribed
+                    //TODO: Remove this user's subscriptions from the DB
+                    break;
+                }
+
+            }
+        } else {
+            // We have all the required data, send back response
+        }
     }
 }
