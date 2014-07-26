c3211756ba6cce20451b384d82d35c264c73a9cc
Grant Lucas <contact@grantlucas.com>
2014-07-26 16:16:02 -0400

    added check to ensure from and body are set for start

--- src/Controller/Subscribe.php
+++ src/Controller/Subscribe.php
@@ -100,6 +100,9 @@ class Subscribe
         //$message     = "Subscribe to stop 1234 for route 54 at 9 am monday.";
         $from_number = isset($_GET['From']) ? $_GET['From'] : null;
 
+        if(empty($message) || $empty($from_number))
+            $app->halt(403);
+
         // Parse out the stop number from the message
         $stop_pattern = "/stop\ *([1-9]+)/i";
         $stop_matches = array();
